74bfb55643a3b25b6d255e75b115935bef33fa5e
conor <conorklamann@gmail.com>
2021-12-21 12:37:07 -0500

    poc for updating node in force graph

--- react/src/ForceGraph/ForceGraph.tsx
+++ react/src/ForceGraph/ForceGraph.tsx
@@ -1,18 +1,16 @@
-import {
-  BaseType,
-  hierarchy,
-  HierarchyNode,
-  select,
-  Selection,
-  SimulationLinkDatum,
-  SimulationNodeDatum,
-} from "d3";
+import { hierarchy, HierarchyNode } from "d3-hierarchy";
+import { select } from "d3-selection";
+import { transition } from "d3-transition";
+//import { transition } from "d3";
+
 import {
   forceSimulation,
   forceLink,
   forceManyBody,
   forceX,
   forceY,
+  SimulationLinkDatum,
+  SimulationNodeDatum,
 } from "d3-force";
 import React, { useEffect, useLayoutEffect, useState } from "react";
 import getModel, {
@@ -25,6 +23,7 @@ import getModel, {
   ModelEntity,
   Relationship,
 } from "../data/model";
+import { BaseType } from "d3";
 
 const ForceGraph: React.FC<{}> = () => {
   const [model, setModel] = useState<Model>();
@@ -81,12 +80,16 @@ const buildTree = (
   return res;
 };
 
+interface ForceNode extends HierarchicalNode {
+  selected?: boolean;
+}
+
 /* for correct tying for data annotated with coordinates by forceLink/simulation, we need to extend these interfaces */
 interface ForceNodeWrapper<T> extends HierarchyNode<T>, SimulationNodeDatum {}
 interface ForceLinkWrapper<T> extends SimulationLinkDatum<T> {}
 
 const buildForceGraph = (
-  _nodes: HierarchicalNode,
+  _nodes: ForceNode,
   selector: string,
   width: number,
   height: number
@@ -96,17 +99,18 @@ const buildForceGraph = (
   const nodes = root.descendants();
 
   const forceLinks = forceLink<
-    ForceNodeWrapper<HierarchicalNode>,
-    ForceLinkWrapper<ForceNodeWrapper<HierarchicalNode>>
+    ForceNodeWrapper<ForceNode>,
+    ForceLinkWrapper<ForceNodeWrapper<ForceNode>>
   >(links)
     .distance(0)
     .strength(1);
 
-  const simulation = forceSimulation<ForceNodeWrapper<HierarchicalNode>>(nodes)
+  const simulation = forceSimulation<ForceNodeWrapper<ForceNode>>(nodes)
     .force("link", forceLinks)
     .force("charge", forceManyBody().strength(-25))
     .force("x", forceX())
-    .force("y", forceY());
+    .force("y", forceY())
+    .velocityDecay(0.8); //higher is faster, default is .4
 
   const svg = select(`#${selector}`)
     .append("svg")
@@ -116,11 +120,37 @@ const buildForceGraph = (
 
   const link = svg
     .append("g")
-    .attr("stroke", "#999")
     .attr("stroke-opacity", 0.6)
     .selectAll("line")
     .data(forceLinks.links())
-    .join("line");
+    .join("line")
+    .attr("stroke", (d) =>
+      (d.target as ForceNodeWrapper<ForceNode>).data.selected &&
+      (d.target as ForceNodeWrapper<ForceNode>).data.selected
+        ? "red"
+        : "black"
+    );
+
+  setTimeout(() => {
+    const _nodes = nodes.map((n) => {
+      n.data.entity.name === "Gary Bader"
+        ? (n.data.selected = true)
+        : (n.data.selected = false);
+      return n;
+    });
+
+    /* would be great to add radial force away from selected node, also, animate the transition to red (make big then small) */
+    node
+      .data(_nodes)
+      .join("circle")
+      .attr("fill", (d) =>
+        d.children ? null : d.data.selected ? "red" : "black"
+      )
+      .attr("stroke", (d) => (d.children ? null : "#fff"))
+      .attr("r", (d) => (d.data.selected ? 7 : 3.5));
+
+    simulation.alpha(0.4).restart();
+  }, 5000);
 
   const node = svg
     .append("g")
@@ -130,7 +160,9 @@ const buildForceGraph = (
     .selectAll("circle")
     .data(simulation.nodes())
     .join("circle")
-    .attr("fill", (d) => (d.children ? null : "#000"))
+    .attr("fill", (d) =>
+      d.children ? null : d.data.selected ? "red" : "black"
+    )
     .attr("stroke", (d) => (d.children ? null : "#fff"))
     .attr("r", 3.5);
   //.call(drag(simulation));
@@ -139,10 +171,10 @@ const buildForceGraph = (
 
   simulation.on("tick", () => {
     link
-      .attr("x1", (d) => (d.source as ForceNodeWrapper<HierarchicalNode>).x!)
-      .attr("y1", (d) => (d.source as ForceNodeWrapper<HierarchicalNode>).y!)
-      .attr("x2", (d) => (d.target as ForceNodeWrapper<HierarchicalNode>).x!)
-      .attr("y2", (d) => (d.target as ForceNodeWrapper<HierarchicalNode>).y!);
+      .attr("x1", (d) => (d.source as ForceNodeWrapper<ForceNode>).x!)
+      .attr("y1", (d) => (d.source as ForceNodeWrapper<ForceNode>).y!)
+      .attr("x2", (d) => (d.target as ForceNodeWrapper<ForceNode>).x!)
+      .attr("y2", (d) => (d.target as ForceNodeWrapper<ForceNode>).y!);
 
     node.attr("cx", (d) => d.x!).attr("cy", (d) => d.y!);
   });
--- react/src/index.tsx
+++ react/src/index.tsx
@@ -7,7 +7,7 @@ const App: React.FC = () => (
   <div
     style={{ display: "flex", alignItems: "center", justifyContent: "center" }}
   >
-    {/* <NetworkViz />  */}
+    {/* <NetworkViz /> */}
     {<ForceGraph />}
   </div>
 );

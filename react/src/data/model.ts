41fd56dbd1c6c12a8bf68fe1e818e91be3ae008b
conor <conorklamann@gmail.com>
2022-01-13 15:44:05 -0500

    refactor model to remove links in favor d3.stratify

--- react/src/data/model.ts
+++ react/src/data/model.ts
@@ -1,3 +1,4 @@
+import { datatype } from 'faker';
 import {
     AcademicProgram,
     AcademicProgramsDataRaw,
@@ -5,12 +6,42 @@ import {
     Person,
     BaseEntity,
 } from '../types';
-import { uniqueBy, groupBy } from '../util';
+import { getKeys, groupBy, uniqueBy } from '../util';
 
 const fetchAcademicProgramsData = async () => {
-    return (await (await fetch('academic-programs.json')).json()) as Promise<
-        AcademicProgramsDataRaw[]
-    >;
+    return (await (
+        await fetch('dsi-network/academic-programs.json')
+    ).json()) as Promise<AcademicProgramsDataRaw[]>;
+};
+
+export type Relationship =
+    | 'affiliate'
+    | 'department'
+    | 'division'
+    | 'fellow'
+    | 'graduate_student'
+    | 'grantee'
+    | 'postdoc'
+    | 'principal_investigator'
+    | 'professor'
+    | 'program'
+    | 'researcher'
+    | 'staff'
+    | 'support'
+    | 'undergraduate';
+
+export interface EntityDict {
+    campus: Campus[];
+    division: Division[];
+    person: Person[];
+    program: AcademicProgram[];
+    unit: Unit[];
+}
+
+const initialEntityAttributes = {
+    parentId: null,
+    parentType: null,
+    relationship: null,
 };
 
 const yesToBool = (str?: string) =>
@@ -30,199 +61,117 @@ const transformPrograms = (data: AcademicProgramsDataRaw[]) => {
                 ? `${u.program} (${u.campus})`
                 : u.program,
         type: 'program' as const,
+        ...initialEntityAttributes,
     }));
 };
 
 const fetchPeopleData = async () => {
-    return (await (await fetch('people.json')).json()) as Promise<
+    return (await (await fetch('dsi-network/people.json')).json()) as Promise<
         PersonDataRaw[]
     >;
 };
 
-const transformPeople = (data: PersonDataRaw[]) =>
-    data.map((u, id) => ({
-        ...Object.fromEntries(
-            Object.entries(u).map(([k, v]) => [
-                k,
-                k.endsWith('_bool') ? !!v : v,
-            ])
-        ),
-        id: id + 1,
-        name: `${u.first} ${u.last}`,
-        type: 'person',
-    })) as Person[];
+/* 
+    create separate person entity for primary and secondary roles and replace boolean string flags with `true`
+*/
+const makePerson = (type: 'primary' | 'secondary', person: PersonDataRaw) => {
+    const ret = { ...initialEntityAttributes } as any;
+    const filterStr = type === 'primary' ? 'secondary' : 'primary';
+
+    getKeys(person).forEach(k => {
+        if (!k.startsWith(filterStr)) {
+            let p: string = k;
+            if (k.startsWith(type)) {
+                p = k.replace(`${type}_`, '');
+            }
+            ret[p as keyof Person] = k.toString().endsWith('_bool')
+                ? !!person[k]
+                : person[k];
+        }
+    });
+
+    ret.id = datatype.uuid();
+    ret.name = `${ret.first} ${ret.last}`;
+    ret.type = 'person';
+
+    return ret as Person;
+};
+
+const transformPerson = (person: PersonDataRaw) => [
+    person.primary_role ? makePerson('primary', person) : null,
+    person.secondary_role ? makePerson('secondary', person) : null,
+];
 
 export type Campus = BaseEntity;
 
-export interface Division extends BaseEntity {
-    campusId?: number;
-}
+export type Division = BaseEntity;
 
-export interface Unit extends BaseEntity {
-    divisionId?: number;
-}
-
-export interface EntityDict {
-    campus: Campus[];
-    division: Division[];
-    person: Person[];
-    program: AcademicProgram[];
-    unit: Unit[];
-}
-
-export interface Model extends EntityDict {
-    links: Link[];
-}
-
-export type Relationship =
-    | 'affiliate'
-    | 'department'
-    | 'division'
-    | 'fellow'
-    | 'graduate_student'
-    | 'grantee'
-    | 'postdoc'
-    | 'principal_investigator'
-    | 'professor'
-    | 'program'
-    | 'researcher'
-    | 'staff'
-    | 'support'
-    | 'undergraduate';
+export type Unit = BaseEntity;
 
 export type EntityType = keyof EntityDict;
 
-export interface Link {
-    uId: number;
-    uType: EntityType;
-    vId: number;
-    vType: EntityType;
-    relationship: Relationship;
-}
+// people should be limited to these roles already
+const getPersonRelationship = (role: string): Relationship =>
+    role.toLowerCase().includes('professor')
+        ? 'professor'
+        : 'principal_investigator';
 
-const createDivisionsLinksAndUnits = (
-    programs: AcademicProgram[],
-    people: Person[]
-) => {
-    const links: Link[] = [];
-    const campus = [...new Set(programs.map(u => u.campus))]
+const transformDepartmentName = (name: string) =>
+    (name || '')
+        .toLowerCase()
+        .replace(/(,|(department of))/g, '')
+        .replace('&', 'and')
+        .trim();
+
+/* mutates program and people entities by adding parent identifiers and splitting people by role */
+const linkEntities = (programs: AcademicProgram[], people: Person[]) => {
+    const campuses: Campus[] = [...new Set(programs.map(u => u.campus))]
         .filter(Boolean)
         .map((d, id) => ({
             name: d,
             id: id + 1,
             type: 'campus' as const,
+            ...initialEntityAttributes,
         }));
 
-    const division: Division[] = programs
-        .map(p => ({
-            id: p.id,
-            name: p.division,
-            campusId: campus.find(c => c.name === p.campus)?.id,
+    const divisions = programs
+        .filter(uniqueBy('division'))
+        .map((d, i) => ({
+            id: i + 1,
+            name: d.division,
             type: 'division' as const,
+            parentId: campuses.find(c => c.name === d.campus)?.id,
+            parentType: 'campus',
+            relationship: 'division',
         }))
-        .filter(uniqueBy('name'));
+        .filter(d => !!d.name && d.parentId) as Division[];
 
-    const unit: Unit[] = programs
-        .filter(p => !!p.unit)
-        .map(p => ({
-            id: p.id,
-            name: p.unit!,
-            divisionId: division.find(d => d.name === p.division)?.id,
+    const units = programs
+        .filter(uniqueBy('unit'))
+        .map((u, i) => ({
+            id: i + 1,
+            name: u.unit,
             type: 'unit' as const,
+            parentId: divisions.find(d => d.name === u.division)?.id,
+            parentType: 'division',
+            relationship: 'unit',
         }))
-        .filter(uniqueBy('name'));
-
-    // people should be limited to these roles already
-    const getPersonRelationship = (_role?: string): Relationship => {
-        const role = (_role || '').toLowerCase();
-        if (role.includes('professor')) {
-            return 'professor';
-        } else return 'principal_investigator';
-    };
-
-    const transformDepartmentName = (name: string) =>
-        (name || '')
-            .toLowerCase()
-            .replace(/(,|(department of))/g, '')
-            .replace('&', 'and')
-            .trim();
-
-    const populateUnit = (unit: Unit, people: Person[]) => {
-        // just doing department to department for now
-
-        const _people = people.map(person => {
-            const link = {
-                uId: unit.id,
-                uType: 'unit' as EntityType,
-                vId: person.id,
-                vType: 'person' as EntityType,
-                relationship: getPersonRelationship(
-                    transformDepartmentName(person.primary_department) ===
-                        transformDepartmentName(unit.name)
-                        ? person.primary_role
-                        : person.secondary_role
-                ),
-            };
-            links.push(link);
-            return person;
-        });
-        return _people;
-    };
-
-    const createLink = (
-        uId: number,
-        vId: number,
-        uType: EntityType,
-        vType: EntityType,
-        relationship: Relationship
-    ) => {
-        const link = {
-            uId,
-            vId,
-            uType,
-            vType,
-            relationship,
-        };
-        links.push(link);
-        return link;
-    };
-
-    //campus is parent of division
-    division.forEach(d => {
-        if (d.name && d.campusId) {
-            createLink(d.campusId, d.id, 'campus', 'division', 'division');
-        }
-    });
+        .filter(u => !!u.name && u.parentId) as Unit[];
 
     /* filter out all but PIs and professors and map to departments */
     const departmentMap = people.reduce<{ [key: string]: [Person] }>(
         (acc, curr) => {
-            const primaryDepartment = transformDepartmentName(
-                curr.primary_department
-            );
-            const secondaryDepartment = transformDepartmentName(
-                curr.secondary_department
-            );
-            if (
-                curr.primary_role &&
-                primaryDepartment &&
-                (curr.primary_role.toLowerCase().includes('professor') ||
-                    curr.primary_role.toLowerCase().includes('investigator'))
-            ) {
-                acc[primaryDepartment]
-                    ? acc[primaryDepartment].push(curr)
-                    : (acc[primaryDepartment] = [curr]);
-            }
+            const department = transformDepartmentName(curr.department);
 
             if (
-                curr.secondary_role &&
-                secondaryDepartment &&
-                (curr.secondary_role.toLowerCase().includes('investigator') ||
-                    curr.secondary_role.toLowerCase().includes('professor'))
+                curr.role &&
+                department &&
+                (curr.role.toLowerCase().includes('professor') ||
+                    curr.role.toLowerCase().includes('investigator'))
             ) {
-                acc[secondaryDepartment]
-                    ? acc[secondaryDepartment].push(curr)
-                    : (acc[secondaryDepartment] = [curr]);
+                acc[department]
+                    ? acc[department].push(curr)
+                    : (acc[department] = [curr]);
             }
 
             return acc;
@@ -230,109 +179,68 @@ const createDivisionsLinksAndUnits = (
         {}
     );
 
-    // divisions are parents of units (assuming for now units are all departments)
     // people data doesn't typically map to programs (e.g., a major), so we'll attach to units instead
-    unit.forEach(u => {
-        // link department to division
-        if (u.divisionId) {
-            createLink(u.divisionId, u.id, 'division', 'unit', 'department');
-        }
+    const linkedPeople = units.flatMap(u => {
         //add people to department
-        if (departmentMap[transformDepartmentName(u.name)]) {
-            populateUnit(u, departmentMap[transformDepartmentName(u.name)]);
-        }
-    });
+        return departmentMap[transformDepartmentName(u.name)]
+            ? departmentMap[transformDepartmentName(u.name)].map(p => ({
+                  ...p,
+                  parentType: 'unit',
+                  parentId: u.id,
+                  relationship: getPersonRelationship(p.role),
+              }))
+            : [];
+    }) as Person[];
 
-    // units (departments) are parents of programs -- try linking there first, before division
-    programs.forEach(p => {
-        let divisionId: number | undefined;
-        const unitId = p.unit ? unit.find(u => u.name === p.unit)?.id : null;
-        if (unitId) {
-            createLink(unitId, p.id, 'unit', 'program', 'program');
-        } else {
-            divisionId = division.find(d => p.division === d.name)?.id;
-        }
-        if (divisionId) {
-            createLink(divisionId, p.id, 'division', 'program', 'program');
-        }
-    });
+    // units (departments) are parents of programs -- try linking there first, then division
+    // each can have only one parent
+    const filteredPrograms = programs
+        .map(p => {
+            let divisionId: number | undefined;
+            const unitId = p.unit
+                ? units.find(u => u.name === p.unit)?.id
+                : null;
+            if (unitId) {
+                return {
+                    ...p,
+                    parentId: unitId,
+                    parentType: 'unit',
+                    relationship: 'department',
+                };
+            } else {
+                divisionId = divisions.find(d => p.division === d.name)?.id;
+            }
+            if (divisionId) {
+                return {
+                    ...p,
+                    parentId: divisionId,
+                    parentType: 'division',
+                    relationship: 'division',
+                };
+            } else return false;
+        })
+        .filter(p => !!p && p.parentId) as AcademicProgram[];
 
-    return { campus, division, links, unit };
+    return [
+        ...filteredPrograms,
+        ...units,
+        ...divisions,
+        ...campuses,
+        ...linkedPeople,
+    ];
 };
-const getModel = async (): Promise<Model> => {
-    const programData = await fetchAcademicProgramsData();
-    const program = transformPrograms(programData);
-
+const getModel = async (): Promise<ModelEntity[]> => {
     const peopleData = await fetchPeopleData();
-    const person = transformPeople(peopleData);
+    const people = peopleData
+        .flatMap(transformPerson)
+        .filter(Boolean) as Person[];
 
-    const results = {
-        person,
-        program,
-        ...createDivisionsLinksAndUnits(program, person),
-    };
+    const programData = await fetchAcademicProgramsData();
+    const programs = transformPrograms(programData);
 
-    return results;
+    return linkEntities(programs, people);
 };
 
 export type ModelEntity = Unit | Division | Person | AcademicProgram;
-
-//todo: d3.hierarchy can probably take it from here
-export interface HydratedLink {
-    child: ModelEntity;
-    childType: EntityType;
-    id: number;
-    parent: ModelEntity;
-    parentType: EntityType;
-    relationship: Relationship;
-}
-
-type ModelMap = { [K in keyof Model]: { [id: number]: ModelEntity } };
-
-export const getKeys = <T>(obj: T) => Object.keys(obj) as (keyof T)[];
-
-export const hydrateLinks = (model: Model) => {
-    const { links, ...modelsToHydrate } = model;
-
-    // this is slow, needss to be rethought/optimized
-
-    const modelMap = getKeys(modelsToHydrate).reduce<ModelMap>(
-        (acc, k) => ({
-            ...acc,
-            [k]: Object.values(modelsToHydrate[k]).reduce<{
-                [id: number]: ModelEntity;
-            }>(
-                (acc, curr: ModelEntity) => ({
-                    ...acc,
-                    [curr.id]: curr,
-                }),
-                {}
-            ),
-        }),
-        {} as ModelMap
-    );
-
-    return links.map((l, i) => ({
-        child: modelMap[l.vType][l.vId],
-        childType: l.vType,
-        id: i + 1,
-        parent: modelMap[l.uType][l.uId],
-        parentType: l.uType,
-        relationship: l.relationship,
-    }));
-};
-
-export interface HierarchicalLeafNode {
-    relationship: Relationship;
-    value: number;
-}
-
-export type HierarchicalNodeChild = HierarchicalNode | HierarchicalLeafNode;
-export interface HierarchicalNode {
-    children: HierarchicalNodeChild[];
-    entity: ModelEntity;
-    relationToParent: Relationship | 'root';
-    type: EntityType;
-}
 
 export default getModel;

69d8081a8f44b4673994764a7d4e3a241cdb7aa9
cklamann <12862284+cklamann@users.noreply.github.com>
2022-04-12 16:50:28 -0400

    fix .htaccess filename

--- react/.eslintrc.js
+++ react/.eslintrc.js
@@ -5,40 +5,43 @@ module.exports = {
         es2021: true,
     },
     extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
     ],
-    parser: "@typescript-eslint/parser",
+    react: {
+        version: 'latest',
+    },
+    parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
         },
         ecmaVersion: 13,
-        sourceType: "module",
+        sourceType: 'module',
     },
-    plugins: ["@typescript-eslint", "import", "react", "react-hooks"],
+    plugins: ['@typescript-eslint', 'import', 'react', 'react-hooks'],
     rules: {
-        "no-console": "warn",
-        "no-debugger": "off",
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn",
-        "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/no-this-alias": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "import/order": [
-            "error",
+        'no-console': 'warn',
+        'no-debugger': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-this-alias': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'import/order': [
+            'error',
             {
-                "pathGroups": [
+                pathGroups: [
                     {
-                        "pattern": "react",
-                        "group": "builtin",
-                        "position": "before"
-                    }
+                        pattern: 'react',
+                        group: 'builtin',
+                        position: 'before',
+                    },
                 ],
-                "pathGroupsExcludedImportTypes": ["react"]
-            }
+                pathGroupsExcludedImportTypes: ['react'],
+            },
         ],
-        "import/no-useless-path-segments": "error"
+        'import/no-useless-path-segments': 'error',
     },
 };
